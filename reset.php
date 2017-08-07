704deda744946ac1a183363d5a27e4ba4ef4f179
Sa'ar <geger40.1@gmail.com>
2017-08-06 22:16:52 -0400

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,23 @@
+<?php
+	var email = $_POST["emailAddress"];
+?>
+
+<script src="https://www.gstatic.com/firebasejs/4.2.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-auth.js"></script>
+<script>
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyB3NfuY86SMNNlX4s35-6ZvKRemya_WewU",
+		authDomain: "starfish-support.firebaseapp.com",
+		databaseURL: "https://starfish-support.firebaseio.com",
+		projectId: "starfish-support",
+		storageBucket: "starfish-support.appspot.com",
+		messagingSenderId: "834983002476"
+	};
+	firebase.initializeApp(config);
+</script>
+<script>
+	function firebaseHandling() {
+	
+}
